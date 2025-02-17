4c2f8f327ae7b9ddf8a574a941dafc43779be6b8
Merari Argueta <mgarguetar@unah.hn>
2025-02-16 22:26:43 -0600

    Quick Creation

--- welcome_to_reminders/lib/main.dart
+++ welcome_to_reminders/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -41,17 +43,58 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Colors.white38,
       ),
-      body: Center(
-        child: Column(
-         children: [
-          Text('Welcome to Reminders', 
+       body: Column(
+      children: [
+        Text(
+          'Welcome to Reminders',
           style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center, 
-          ),
-          SizedBox(height: 45),
-         ],
+          textAlign: TextAlign.center,
         ),
-      ),
-    );
+        SizedBox(height: 45),
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Primera columna de iconos
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.timelapse, color: Colors.green),
+                  Icon(Icons.flag, color: Colors.green),
+                ],
+              ),
+              // Segunda columna de iconos (Send y Camera)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Icon(Icons.camera_alt, color: Colors.green),
+                  Icon(Icons.send, color: Colors.green),
+                ],
+              ),
+              SizedBox(width: 15),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                Text(
+                  'Quick Creation',
+                  style: TextStyle(fontSize: 15, color: Colors.black, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Simple type in your list, ask Siri, or add',
+                  style: TextStyle(fontSize: 15, color: Colors.blueGrey),
+                ),
+                Text(
+                  'a reminder from your Calendar app.',
+                  style: TextStyle(fontSize: 15, color: Colors.blueGrey),
+                ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
 }
